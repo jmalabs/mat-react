e44b5a82e09929aa0fd7bd10c7e7e1f222ab8081
jmalabs <johnrichard.malabanan.0916@gmail.com>
2019-08-17 22:30:24 +0800

    base layout

--- src/components/Exercises/index.js
+++ src/components/Exercises/index.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Grid, Paper } from "@material-ui/core";
+import LeftPane from './LeftPane'
+import RightPane from './RightPane'
+
+const styles = {
+
+  Paper: {padding: 10, margin: 10}
+
+}
 
 export default props => (
-    
-    <div>Exercises</div>
-
-)
+  <Grid container>
+    <Grid item sm>
+      <LeftPane styles={styles.Paper}/>
+    </Grid>
+    <Grid item sm>
+     <RightPane styles={styles.Paper} />
+    </Grid>
+  </Grid>
+);
--- src/components/Layouts/Header.js
+++ src/components/Layouts/Header.js
@@ -1,7 +1,17 @@
 import React from 'react'
-
+import {AppBar, Toolbar, Typography} from '@material-ui/core';
+// import Typography from '@material-ui/core/Typography';
+// import Button from '@material-ui/core/Button';
+// import IconButton from '@material-ui/core/IconButton';
+// import MenuIcon from '@material-ui/icons/Menu';
 
 export default props => (
-<div>Header</div>
+    <AppBar position="static">
+    <Toolbar>
+      <Typography variant="h5" color="inherit">
+      Exercises
+      </Typography>
+    </Toolbar>
+  </AppBar>
 
 )
